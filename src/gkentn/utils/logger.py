57f453ee11691c33f68502f035bb4de5d5805d65
gumigumi4f <gumigumi4f@pixiv.co.jp>
2022-12-27 11:38:47 +0900

    Change log level

--- src/gkentn/utils/logger.py
+++ src/gkentn/utils/logger.py
@@ -4,8 +4,13 @@ import logging
 def get_logger() -> logging.Logger:
     logger = logging.getLogger(__name__)
     handler = logging.StreamHandler()
+
+    handler.setLevel(logging.INFO)
+    logger.setLevel(logging.INFO)
+
     handler.setFormatter(
         logging.Formatter(fmt="%(asctime)s [%(levelname)s] %(message)s", datefmt="%Y-%m-%d %H:%M:%S %z")
     )
     logger.addHandler(handler)
+
     return logger
